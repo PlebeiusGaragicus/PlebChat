1de9ec39361010e4dca344560ed00ad443a4376b
PlebbyG <plebeiusgaragicus@gmail.com>
2024-01-15 15:25:17 -0800

    favicon

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+import pathlib
 import yaml
 
 import streamlit as st
@@ -9,20 +11,27 @@ log = logging.getLogger()
 from src.common import init_if_needed, PageRoute
 from src.main_page import main_page
 from src.settings_page import settings_page
+import pathlib
+import yaml
 
+# ASSETS_PATH = pathlib.Path(__file__).parent / "assets"
+ASSETS_PATH = pathlib.Path(__file__).parent.parent / "assets"
 
 
 def main():
     st.set_page_config(
         page_title="Pleb Chat",
+        page_icon=os.path.join(ASSETS_PATH, "favicon.ico"),
         layout="centered", # vs wide
         initial_sidebar_state="auto",
         # menu_items={"About": "https://plebby.me/"} # TODO
     )
 
+    # st.image(os.path.join(ASSETS_PATH, "favicon.ico"))
+
     try:
         with open("./auth.yaml") as file:
-            config = yaml.load(file, Loader=yaml.loader.SafeLoader)
+            config = yaml.safe_load(file)
     except FileNotFoundError:
         st.error("This instance of PlebChat has not been configured.  Missing `auth.yaml` file.")
         st.stop()
